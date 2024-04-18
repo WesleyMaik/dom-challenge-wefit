5e695760b4ccb77efdb3731229f6440821911559
WesleyMaik <wesleynamikaze404@gmail.com>
2024-04-18 11:41:56 -0400

    feat: add changeHeaderAlignment function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const menu = document.querySelector(".btn-group-vertical");
+const header = document.querySelector(".jumbotron");
 
 function setMenuToHorizontal(menu) {
 	if (!menu) return;
@@ -6,4 +7,31 @@ function setMenuToHorizontal(menu) {
 	menu.classList.replace("btn-group-vertical", "btn-group-horizontal");
 }
 
+function changeHeaderAlignment(header) {
+	if (!header) return;
+
+	header.classList.add("text-white");
+	header.classList.add("bg-secondary");
+
+	const texts = header.querySelectorAll("h1, p");
+	const button = header.querySelector(".btn");
+
+	texts.forEach((element) => {
+		element.style.textAlign = "right";
+	});
+
+	const buttonStyles = {
+		display: "block",
+		width: "fit-content",
+		marginLeft: "auto",
+	};
+
+	Object.keys(buttonStyles).forEach((style) => {
+		button.style[style] = buttonStyles[style];
+	});
+
+	button.classList.replace("btn-primary", "btn-success");
+}
+
 setMenuToHorizontal(menu);
+changeHeaderAlignment(header);
